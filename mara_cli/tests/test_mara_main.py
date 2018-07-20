f2344889f192d3f7dde52cc2f0b61f53b5449d4e
Jan Katins <jan.katins@kfzteile24.de>
2018-07-21 01:05:09 +0200

    Fix tests

--- mara_cli/tests/test_mara_main.py
+++ mara_cli/tests/test_mara_main.py
@@ -8,5 +8,4 @@ def test_without_argument(cli_runner):
     # here we get the name as 'cli' instead of 'mara'
     assert 'Usage: cli [OPTIONS] COMMAND [ARGS]' in result.output
     assert re.search(r'--debug\s+Show debug output',result.output) is not None
-    assert re.search(r'print_config\s+Prints the current config', result.output) is not None
 
